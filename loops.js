12cb19adfd93045d6a7fe92f200bcb431d9e8cd1
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-16 23:38:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,1 @@
-var gifts = ['teddy bear', 'drone', 'doll']
- 
-function wrapGift(gift) {
-  console.log(`Wrapped ${gifts} and added a bow!`)
-}
-
-
-
-console.log(`Hello ${gifts}`)
+function forLoop ()
